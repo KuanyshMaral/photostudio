322723a4a98d8dba766b32ac3415a8009a38063f
vanga <vangamanga7@gmail.com>
2025-12-24 16:29:06 +0500

    feat(auth): add mock POST /auth/login endpoint

--- internal/modules/auth/handler.go
+++ internal/modules/auth/handler.go
@@ -1,1 +1,30 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type jwtService interface {
+	GenerateToken(userID int64, role string) (string, error)
+}
+
+type Handler struct {
+	jwt jwtService
+}
+
+func NewHandler(jwt jwtService) *Handler {
+	return &Handler{jwt: jwt}
+}
+
+func (h *Handler) Login(c *gin.Context) {
+	token, _ := h.jwt.GenerateToken(1, "studio_owner")
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data": gin.H{
+			"token": token,
+		},
+	})
+}
